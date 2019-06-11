4625f153c50a9bff6146641b38d5ea6e6dd3aa17
Maggie <858123145@qq.com>
2019-06-11 23:57:13 +0800

    第二个版本

--- mian.js
+++ mian.js
@@ -1,17 +1,15 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+初始化()
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n+1))
   n += 1
 },3000)
 
-
+//下面都是封装之后的函数，可以不看
 function x(n){
   if(n>3){
     n = n%3
@@ -20,4 +18,26 @@ function x(n){
     }
   } // n = 1 2 3
   return n
+}
+
+function 初始化(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
+
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
 }
